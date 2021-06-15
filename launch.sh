91356d8d279e6c7e54beda3ff470242f0fb61abe
sande182 <58714789+sande182@users.noreply.github.com>
2021-06-15 15:52:38 -0500

    First revision of bash script

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./build/chia_plot -n -1 \
+	-r 23\
+       	-u 256\
+       	-t /media/christian/Plotter/ \
+       	-2 /mnt/ram/ \
+	-d /media/christian/Plotter/madmax/\
+       	-p 9597b9464f2debafb3946725b96bc14b78500a302d822a4d366d46496111df7ecf4f78120cde77cb70485f4782e6e39e \
+	-f 96172c2e48983d061bf800c492e02cf11fad304390add34131270ac6b073116c6026e8b23a36484a9e3c1bb9795fb90f
+
